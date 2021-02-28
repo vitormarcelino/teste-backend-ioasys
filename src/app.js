14b22f7d1b52a0806ebda82a91e7854d75c9b3f3
Vitor Marcelino <vitormarcelino93@gmail.com>
2021-02-27 22:01:39 -0300

    fix: Organization of middlewares

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import express from 'express'
 import apiRouter from './routes/api';
-import authMiddleware from './middlewares/auth'
 
 const app = express()
-app.use(authMiddleware)
 app.use(express.json())
 app.use('/', apiRouter)
 
--- src/routes/api.js
+++ src/routes/api.js
@@ -3,10 +3,12 @@ import AdminController from '../controllers/adminController.js'
 import AuthController from '../controllers/authController.js'
 import UserController from '../controllers/userController.js'
 import MovieController from '../controllers/movieController.js'
+import authMiddleware from '../middlewares/auth'
 import isAdminMiddleware from '../middlewares/isAdmin'
 import isUserMiddleware from '../middlewares/isUser'
 
 var router = express.Router()
+router.use(authMiddleware)
 
 const authController = new AuthController()
 
@@ -14,13 +16,11 @@ const authController = new AuthController()
 #### Admin Routes ####
 */
 const adminController = new AdminController()
-router.get('/admins', adminController.index)
-router.post('/admin', adminController.store)
-router.get('/admin/:id', adminController.show)
-router.patch('/admin/:id', adminController.update)
-router.delete('/admin/:id', adminController.destroy)
-router.post('/admin/authenticate', authController.authAdmin)
-
+router.get('/admins', isAdminMiddleware, adminController.index)
+router.post('/admin', isAdminMiddleware, adminController.store)
+router.get('/admin/:id', isAdminMiddleware, adminController.show)
+router.patch('/admin/:id', isAdminMiddleware, adminController.update)
+router.delete('/admin/:id', isAdminMiddleware, adminController.destroy)
 
 /*
 #### User Routes ####
