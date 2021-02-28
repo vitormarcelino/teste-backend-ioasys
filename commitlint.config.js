e2ea238d0403b04c1cc03c1f60503ef511338993
Vitor Marcelino <vitormarcelino93@gmail.com>
2021-02-27 22:44:01 -0300

    style: Add commit lint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case']],
+    }
+}
